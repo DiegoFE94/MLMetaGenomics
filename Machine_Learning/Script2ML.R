1d6649ab0809069a917581e02396116f48decc53
Diego FE <diegolas94@hotmail.com>
2020-11-06 10:20:02 +0100

    Update Script2ML.R

--- Machine_Learning/Script2ML.R
+++ Machine_Learning/Script2ML.R
@@ -1,4 +1,4 @@
-###  TFM DIABINMUNE_Script4 ###
+###  Script_Machine_Learning_Todos_Algortimos ###
 results = machnlearn(path = "TFM/Datasets_FS/", patron = "DatasetGeneroCase_Control_kruskaltest", outputfile = "TFM/Basura/")
 machnlearn = function(path, patron, outputfile){
   #CARGADATOS
@@ -99,34 +99,6 @@ machnlearn = function(path, patron, outputfile){
   return(bench)
 }
 
-varImp.glmnet = function(bmr, n.model){  
-  models = getBMRModels(bmr)
-  glmnet = models[[n.model]]$classif.glmnet.tuned #El nimero en models hace referecncia a la tanda de modelos de difernte numero de features es decir 2,4,6,7..               ,
-  sum.betas = rep(0)
-  for (model in 1:length(glmnet)) {
-    learner.models = getLearnerModel(glmnet[[model]])
-    sum.betas = sum.betas + as.vector((learner.models$learner.model$beta))
-  }
-  names(sum.betas) = glmnet[[n.model]]$features
-  barplot(abs(sum.betas), main = 'Glmnet Variable Importance', xaxt = "n", ylim = c(0,10), col = topo.colors(length(sum.betas)))
-  return(sum.betas)
-}
-
-
-varImp.rf = function(bmr, n.model){
-  models = getBMRModels(bmr)
-  rf = models[[n.model]]$classif.randomForest.tuned
-  sum.imp = rep(0)
-  for (i in 1:length(rf)) {
-    iters = getFeatureImportance(rf[[i]])
-    res = as.matrix(iters$res)
-    sum.imp =+ sum.imp + res
-  }
-  print(length(sum.imp))
-  barplot(sum.imp, main = 'Random Forest Variable Importance', xaxt = "n", col = topo.colors(length(sum.imp)))
-  return(sum.imp)
-}
-
 ## Asignamos los valores a las variables que le vamos a pasar a la funcion ##
 #origen = 'TFM/Datasets_FS/'
 #patrones = 'DatasetFiloGender'
@@ -136,6 +108,3 @@ varImp.rf = function(bmr, n.model){
 #plotBMRSummary(resultado)
 #plotBMRBoxplots(resultado)
 #models = getBMRModels(resultado)
-#glmnet = models[[]]$classif.glmnet.tuned 
-#glmnet
-#varImp.glmnet(bmr = resultado, n.model = 4)
